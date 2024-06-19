c15d9e965b4175ee352b9feded982306b22e5402
JavierChia <javierchiaaaa@gmail.com>
2024-06-19 13:05:47 +0800

    feat: :sparkles: added multi-token claiming

--- contracts/token/staking/reward.sol
+++ contracts/token/staking/reward.sol
@@ -6,38 +6,49 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
-contract RewardClaim is Ownable {
+contract ClaimReward is Ownable {
     using ECDSA for bytes32;
     using MessageHashUtils for bytes32;
 
     address public signer;
-    IERC20 public rewardToken;
+    mapping(address => bool) private claimableTokens;
 
-    event RewardClaimed(address indexed user, uint256 amount);
+    event RewardClaimed(address indexed user, address indexed token, uint256 amount);
 
-    constructor(address _rewardToken) Ownable(msg.sender) {
+    constructor() Ownable(msg.sender) {
         signer = msg.sender;
-        rewardToken = IERC20(_rewardToken);
     }
 
     function setSigner(address _signer) external onlyOwner {
         signer = _signer;
     }
 
-    function setRewardToken(address _rewardToken) external onlyOwner {
-        rewardToken = IERC20(_rewardToken);
+    function addClaimableToken(address _token) external onlyOwner {
+        require(!claimableTokens[_token], "Token is already claimable!");
+        claimableTokens[_token] = true;
     }
-    function claimReward(uint256 amount, bytes memory signature) external {
-        bytes32 messageHash = keccak256(abi.encodePacked(msg.sender, amount));
+
+    function removeClaimableToken(address _token) external onlyOwner {
+        require(claimableTokens[_token], "Token is not currently in the contract's claimable token list!");
+        claimableTokens[_token] = false;
+    }
+
+    function claimReward(address token, uint256 amount, bytes memory signature) external {
+        require(claimableTokens[token], "Token is not currently in the contract's claimable token list!");
+
+        bytes32 messageHash = keccak256(abi.encodePacked(msg.sender, token, amount));
         bytes32 ethSignedMessageHash = messageHash.toEthSignedMessageHash();
 
         address _signer = ethSignedMessageHash.recover(signature);
         require(_signer == signer, "Invalid signature");
 
-        // Transfer the reward using the specified ERC20 token
-        require(rewardToken.transfer(msg.sender, amount), "Token transfer failed");
+        uint256 contractBalance = IERC20(token).balanceOf(address(this));
+        require(contractBalance >= amount, "Not enough tokens of that kind");
 
-        emit RewardClaimed(msg.sender, amount);
+        // Transfer the reward using the specified ERC20 token
+        require(IERC20(token).transfer(msg.sender, amount), "Token transfer failed");
+
+        emit RewardClaimed(msg.sender, token, amount);
     }
 
     // Function to receive Ether. msg.data must be empty
